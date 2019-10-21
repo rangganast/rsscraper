3f737fc9a7beed44f851616c10219a1f437ce798
Rangga Aziz <rangganast@gmail.com>
2019-10-21 12:57:51 +0700

    first commit

--- app.py
+++ app.py
@@ -508,4 +508,4 @@ def detikfeed():
 
 if __name__ == '__main__':
     app.secret_key = 'super secret key'
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)), debug=True)
