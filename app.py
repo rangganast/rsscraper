7dda356f64d4b6185cd2215ace198b515bf0f025
Rangga Aziz <rangganast@gmail.com>
2019-11-01 14:41:39 +0700

    commit

--- app.py
+++ app.py
@@ -1730,18 +1730,18 @@ def feedtribunnews():
     }
 
     month_dict = {
-        '01 ': 'Jan ',
-        '02 ': 'Feb ',
-        '03 ': 'Mar ',
-        '04 ': 'Apr ',
-        '05 ': 'May ',
-        '06 ': 'Jun ',
-        '07 ': 'Jul ',
-        '08 ': 'Aug ',
-        '09 ': 'Sep ',
-        '10 ': 'Oct ',
-        '11 ': 'Nov ',
-        '12 ': 'Dec ',
+        ' 01 ': ' Jan ',
+        ' 02 ': ' Feb ',
+        ' 03 ': ' Mar ',
+        ' 04 ': ' Apr ',
+        ' 05 ': ' May ',
+        ' 06 ': ' Jun ',
+        ' 07 ': ' Jul ',
+        ' 08 ': ' Aug ',
+        ' 09 ': ' Sep ',
+        ' 10 ': ' Oct ',
+        ' 11 ': ' Nov ',
+        ' 12 ': ' Dec ',
     }
 
     check_dict = {
