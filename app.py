ec713bcec93ab079e6c2528e576008edab542623
Rangga Aziz <rangganast@gmail.com>
2020-06-10 14:26:27 +0700

    fix feedtripcanvas

--- app.py
+++ app.py
@@ -584,7 +584,7 @@ def feedtripcanvas():
         d += ' 00:00:00 +0700'
 
         d_ = ' '.join([check_dict.get(i, i) for i in d.split()])
-        day = datetime.datetime.strptime(d_[:-15], '%d %b %Y').strftime('%a')
+        day = datetime.datetime.strptime(d_[:-15], '%d %B %Y').strftime('%a')
         d_final = day + ", " + d
 
         datetimes_.append(d_final)
